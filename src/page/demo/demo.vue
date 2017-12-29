51a66f82573af536ae676874eb3020fee89f109b
李春升 <790756690@qq.com>
2017-12-29 17:37:31 +0800

    + test demo

--- src/page/demo/demo.vue
+++ src/page/demo/demo.vue
@@ -11,13 +11,80 @@
                 </transition>
             </div>
         </div>
-        <div class="demo_table">
+        <div class="demo-table">
             <el-button @click="searchBtn">查询</el-button>
             <el-table :data="tableData3" height="350" border style="width: 100%">
                 <el-table-column prop="date" label="日期" width="180"> </el-table-column>
                 <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
                 <el-table-column prop="address" label="地址"> </el-table-column>
             </el-table>
+        </div>
+        <div class="demo-menu">
+            <el-row class="tac">
+                <el-col :span="12">
+                    <h5>默认颜色</h5>
+                    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template slot="title">分组一</template>
+                                <el-menu-item index="1-1">选项1</el-menu-item>
+                                <el-menu-item index="1-2">选项2</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                                <el-menu-item index="1-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="1-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="1-4-1">选项1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">导航二</span>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">导航三</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-col>
+                <el-col :span="12">
+                    <h5>自定义颜色</h5>
+                    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+                            <el-menu-item-group>
+                                <template slot="title">分组一</template>
+                                <el-menu-item index="1-1">选项1</el-menu-item>
+                                <el-menu-item index="1-2">选项2</el-menu-item>
+                            </el-menu-item-group>
+                            <el-menu-item-group title="分组2">
+                                <el-menu-item index="1-3">选项3</el-menu-item>
+                            </el-menu-item-group>
+                            <el-submenu index="1-4">
+                                <template slot="title">选项4</template>
+                                <el-menu-item index="1-4-1">选项1</el-menu-item>
+                            </el-submenu>
+                        </el-submenu>
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">导航二</span>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-setting"></i>
+                            <span slot="title">导航三</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-col>
+            </el-row>
+
         </div>
     </div>
 </template>
@@ -41,16 +108,20 @@ export default {
         });
 
         Mock.mock("table_list", {
-            "data|1-10": [
-                {
-                    "name": '@name', //模拟名称
-                    "date": "@date('yyyy-MM-dd')", //模拟时间
-                    "address": "@county(true)" //模拟文本
-                }
-            ]
+            "data|1-10": [{
+                "name": '@name', //模拟名称
+                "date": "@date('yyyy-MM-dd')", //模拟时间
+                "address": "@county(true)" //模拟文本
+            }]
         })
     },
     methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        },
         searchBtn() {
             var _this = this;
             $.ajax({
@@ -80,6 +151,7 @@ export default {
         }
     }
 }
+
 </script>
 
 <style>
@@ -96,7 +168,20 @@ export default {
   box-sizing: border-box;
   margin-right: 20px;
 }
-.demo_table {
+
+.demo-table {
+  float: left;
   width: 1000px;
+  height: 520px;
+}
+
+.demo-menu {
+  float: left;
+  width: 1000px;
+  height: 800px;
+}
+
+.demo-menu h5 {
+  font-size: 20px;
 }
 </style>
